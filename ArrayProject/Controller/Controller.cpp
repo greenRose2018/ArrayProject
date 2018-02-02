a69cff6d4321dbce4a7e5f80bac20fb521d0ce2b
greenRose2018 <brifun13@gmail.com>
2018-02-02 08:47:01 -0700

    imported double <limits> to display double

--- ArrayProject/Controller/Controller.cpp
+++ ArrayProject/Controller/Controller.cpp
@@ -17,19 +17,45 @@ Controller:: Controller()
 
 void Controller:: start()
 {
-    
+    //intArray();
+    doubleArray();
+    //stringArray();
     
 }
 int Controller:: intArray()
 {
+    int wholeNumArray [30];
+    
+    for(int i = 0; i < 30; i++)
+    {
+        wholeNumArray[i] = i;
+    }
+    for(int j = 0; j < 30; j++)
+    {
+        cout << wholeNumArray[j] << endl;
+    }
     
     return 0;
 }
 
 double Controller:: doubleArray()
 {
+    int decimalArray[25];
+    int num = 0 ;
+    cout.precision(4);
     
-    return 1.2;
+    for(double decimal = 0.0; num < 25; decimal+=.5)
+    {
+        decimalArray[num] = decimal;
+        num++;
+    }
+    for (int num = 0; num < 25; num++)
+    {
+        cout << fixed << decimalArray[num] << endl;
+    }
+    
+    
+    return 2;
 }
 
 string Controller:: stringArray()
